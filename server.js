365ead339196729a12934a724b22f9d3236199e7
samir-hussein <samirhussein274@gmail.com>
2024-03-14 01:38:00 +0200

    updates

--- server.js
+++ server.js
@@ -1,42 +1,31 @@
-
 /**
  * Module dependencies.
  */
-
-import app from "./app.js";
+import app from "./dist/app.js";
 import http from "http";
-
 /**
  * Get port from environment and store in Express.
  */
-
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
-
 const server = http.createServer(app);
-
 /**
  * Listen on provided port, on all network interfaces.
  */
-
 server.listen(port, () => console.log("listening on http://localhost:" + port));
-
 /**
  * Normalize a port into a number, string, or false.
  */
-
 function normalizePort(val) {
     var port = parseInt(val, 10);
-
     if (isNaN(port)) {
         // named pipe
         return val;
     }
-
     if (port >= 0) {
         // port number
         return port;
     }
-
     return false;
 }
+//# sourceMappingURL=server.js.map
